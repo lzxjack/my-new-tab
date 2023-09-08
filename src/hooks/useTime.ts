b77ccec388288c7c410d77b359306e378b81357a
lzxjack <965555169@qq.com>
2023-09-08 18:33:20 +0800

    update

---
+++ src/hooks/useTime.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export const useTime = () => {
+  const [time, setTime] = useState<{
+    h?: string;
+    m?: string;
+    s?: string;
+  }>({});
+
+  const getTime = useCallback(() => {
+    const timeline = Date.now();
+    const h = new Date(timeline).getHours().toString().padStart(2, '0');
+    const m = new Date(timeline).getMinutes().toString().padStart(2, '0');
+    const s = new Date(timeline).getSeconds().toString().padStart(2, '0');
+    setTime({ h, m, s });
+    requestAnimationFrame(getTime);
+  }, [setTime]);
+
+  useEffect(() => {
+    const requestAnimationID = requestAnimationFrame(getTime);
+    return () => {
+      cancelAnimationFrame(requestAnimationID);
+    };
+  }, [getTime]);
+
+  return { time };
+};
